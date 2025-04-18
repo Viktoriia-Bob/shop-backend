59e84892d2ac11633faaff9209650d54f05733d9
Viktoriia Bobyr <vikabobir32@gmail.com>
2025-04-18 12:02:35 +0300

    init

--- bin/shop-backend.ts
+++ bin/shop-backend.ts
@@ -1,6 +1,5 @@
 #!/usr/bin/env node
 import * as cdk from 'aws-cdk-lib';
-import { ProductServiceStack } from '../lib/product-service/product-service-stack';
 import { ShopBackendStack } from '../lib/shop-backend-stack';
 
 const app = new cdk.App();
