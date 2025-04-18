291b6ce555e367825af339a5fa36d0402c39aae5
Viktoriia Bobyr <vikabobir32@gmail.com>
2025-04-18 12:03:23 +0300

    task-3

--- bin/shop-backend.ts
+++ bin/shop-backend.ts
@@ -1,8 +1,7 @@
 #!/usr/bin/env node
 import * as cdk from 'aws-cdk-lib';
 import { ProductServiceStack } from '../lib/product-service/product-service-stack';
-import { ShopBackendStack } from '../lib/shop-backend-stack';
 
 const app = new cdk.App();
 
-new ShopBackendStack(app, 'ShopBackendStack');
+new ProductServiceStack(app, 'ProductServiceStack');
---
+++ lib/product-service/mock-products.ts
@@ -0,0 +1,5 @@
+export const products = [
+    { id: '1', title: 'The Witcher 3: Wild Hunt', price: 39.99 },
+    { id: '2', title: "Assassin's Creed Shadows", price: 69.99 },
+    { id: '3', title: 'The Last of Us', price: 59.99 },
+];
